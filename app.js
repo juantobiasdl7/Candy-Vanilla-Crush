efd8ea02b34a9cd2a438085e1523253fe039f235
Juan Tobias <juantobiasdl7@gmail.com>
2020-05-13 16:08:11 -0500

    I've created a board with draggable elements.

---
+++ app.js
@@ -0,0 +1,31 @@
+//This allows to display to HTML file first
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    const width = 8;
+    const squares = [];
+
+    const candyColors = [
+        'red',
+        'yellow',
+        'orange',
+        'purple',
+        'green',
+        'blue'
+    ];
+
+    //Create a board
+    function createBoard(){
+        for(let i=0; i<width*width; i++){
+            const square = document.createElement('div');
+            square.setAttribute('draggable', true);
+            square.setAttribute('id', i);
+            let randomColor = Math.floor(Math.random()*candyColors.length);
+            square.style.backgroundColor = candyColors[randomColor];
+            grid.appendChild(square);
+            squares.push(square);
+        }
+    }
+
+    createBoard();
+
+});
